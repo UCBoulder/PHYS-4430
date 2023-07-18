ce89d7fa3c3d232cf49be2c0c725f0261ad39759
Kristopher Bunker <kristopher.bunker@gmail.com>
2023-07-17 18:04:30 -0700

    gb1 test

--- scripts/mdtohtml.ps1
+++ scripts/mdtohtml.ps1
@@ -6,5 +6,5 @@ pandoc ..\markdown\index.md --toc --filter pandoc-xnos --mathjax="https://cdn.ma
 pandoc ..\markdown\lab-issue.md --toc --filter pandoc-xnos --mathjax="https://cdn.mathjax.org/mathjax/..." -s --include-in-header header.html --include-before-body navbar.html --include-after-body footer.html --template ..\templates\lab-issue-template.html -o ..\lab-issue.html
 
 # List of lab guides to generate
-pandoc ..\lab-guides\Gaussian-Laser-Beams\gb1.md --toc --filter pandoc-xnos --mathjax="https://cdn.mathjax.org/mathjax/..." -s -N --include-in-header header.html --include-before-body navbar.html --include-after-body footer.html --template ..\templates\lab-guide-template.html -o ..\gb1.html
+pandoc ..\lab-guides\Gaussian-Laser-Beams\gb1.md --toc --filter pandoc-xnos --mathjax="https://cdn.mathjax.org/mathjax/..." -s -N --include-in-header header.html --include-before-body navbar.html --include-after-body footer.html --template ..\templates\home-template.html -o ..\gb1.html
 pandoc ..\markdown\lab6.md --toc --filter pandoc-xnos --mathjax="https://cdn.mathjax.org/mathjax/..." -s -N --include-in-header header.html --include-before-body navbar.html --include-after-body footer.html --template ..\templates\lab-guide-template.html -o ..\lab6.html
